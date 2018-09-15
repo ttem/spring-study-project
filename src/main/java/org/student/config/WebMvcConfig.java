6ff411394cb82b8b71541b80c0c7a99f99ec4465
1ttem12@gmail.com <Artem52574>
2018-09-15 20:20:20 +0200

    added interceptors mapping

--- src/main/java/org/student/config/WebMvcConfig.java
+++ src/main/java/org/student/config/WebMvcConfig.java
@@ -1,16 +1,45 @@
 package org.student.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.student.interceptor.BadUrlInterceptor;
+import org.student.interceptor.LogInterceptor;
+
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor());
+        registry.addInterceptor(localeChangeInterceptor());
+        registry.addInterceptor(new BadUrlInterceptor()).addPathPatterns("/**").excludePathPatterns("/articles/**")
+                .excludePathPatterns("/login");
+    }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/").setViewName("articles");
     }
 }
