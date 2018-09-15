f8ad37311145bfec3aa6d12c8b4455a4d3310a22
1ttem12@gmail.com <Artem52574>
2018-09-15 10:19:48 +0200

    added Boot

---
+++ src/main/java/org/student/Boot.java
@@ -0,0 +1,10 @@
+package org.student;
+
+import org.springframework.boot.SpringApplication;
+
+public class Boot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot.class, args);
+    }
+}
