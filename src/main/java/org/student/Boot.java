0b10cc3937c69557741dbe44d84c273cff9a3ac3
1ttem12@gmail.com <Artem52574>
2018-09-15 10:36:15 +0200

    application Boot fix

--- src/main/java/org/student/Boot.java
+++ src/main/java/org/student/Boot.java
@@ -1,7 +1,11 @@
 package org.student;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@SpringBootApplication
+@EnableScheduling
 public class Boot {
 
     public static void main(String[] args) {
